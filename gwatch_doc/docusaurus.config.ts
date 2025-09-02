a6b2aaeb9679712df53d9e694cb9ada9834403ed
zobinHuang <zobin1999@gmail.com>
2025-09-02 10:36:59 +0800

    public

--- gwatch_doc/docusaurus.config.ts
+++ gwatch_doc/docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'G-Watch',
-  tagline: 'AI-powered Profiler and Autotuner for Your AI Infrastructure',
+  tagline: 'AI-powered Profiler and Autotuner for Your Infra',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://g-watch.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'G-Watch', // Usually your GitHub org/user name.
+  projectName: 'G-Watch.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
